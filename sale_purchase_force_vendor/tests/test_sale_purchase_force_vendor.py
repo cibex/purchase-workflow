a3774308c6d3d0e783958b39efd291879452b189
joepsanders <joep.sanders@codeforward.com>
2023-08-03 17:16:05 +0200

    [FIX] sale_purchase_force_vendor unused import statement

--- sale_purchase_force_vendor/tests/test_sale_purchase_force_vendor.py
+++ sale_purchase_force_vendor/tests/test_sale_purchase_force_vendor.py
@@ -1,8 +1,6 @@
 # Copyright 2022 Tecnativa - Víctor Martínez
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-import json
-
 from .common import TestSalePurchaseForceVendorBase
 
 
